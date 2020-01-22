fac8f4ddd1f01a804b11a0859a4ef407e41138a0
Armand Iskandarsyah <armand.iskandarsyah@raptorpos.com>
2020-01-22 16:48:21 +0700

    Initial commit

---
+++ database/factories/BarangListFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+
+    $satuan = ['KG', 'LTR', 'ONS'];
+    $user = ['admin', 'Armand', 'Iskandar', 'Eli'];
+
+    // make sure the property same as the model
+    return [
+        'nomor_barang' => $faker->randomNumber(4) .'.'. $faker->randomNumber(4),
+        'nama_barang' => $faker->name,
+        'satuan' => $faker->randomElement($satuan),
+        'kuantitas' => $faker->randomNumber(2),
+        'harga_satuan' => $faker->randomNumber(5),
+        'dibuat_oleh' => $faker->randomElement($user),
+    ];
+});
+
+//
